b532bd96f012f0357056e9cb8f65ace67e184e27
Byron Sommardahl <byron@acklenavenue.com>
2015-06-06 09:58:52 -0500

    Initial commit.

---
+++ src/AcklenAvenue.Poller/PollerState.cs
@@ -0,0 +1,12 @@
+﻿namespace AcklenAvenue.Poller
+{
+    public enum PollerState
+    {
+        Unstarted = 0,
+        Running = 1,
+        StopRequested = 2,
+        Stopped = 3,
+        PauseRequested = 4,
+        Paused = 5,
+    }
+}
